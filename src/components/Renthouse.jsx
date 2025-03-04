91d4ecbb5b479cd44a1e18138132907776ace5e1
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2025-03-04 23:51:49 +0500

    Commit

--- src/components/Renthouse.jsx
+++ src/components/Renthouse.jsx
@@ -110,7 +110,7 @@ const Renthouse = () => {
     }
   
     return (
-        <div className="container mx-auto px-4 py-1">
+        <div className="container mx-auto px-4 py-2">
             <div className="container mx-auto flex md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
